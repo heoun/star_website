e890dde445c7c7d93bf72099218c053caf73fd2e
Augustus <AugustusVash@gmail.com>
2026-09-24 20:28:56 -0400

    fix(lease): align pet checkboxes with party indentation

--- worker/signing-template.js
+++ worker/signing-template.js
@@ -4,7 +4,7 @@ import {splitSigningDocuments,signingVisibleText} from './signing-documents.js';
 import {signingFields,SIGNING_TEMPLATE_VERSION} from '../site/shared/lease-signing-layout.js';
 import {injectSigningAnchors} from './signing-anchors.js';
 export {SIGNING_TEMPLATE_VERSION};
-const SOURCE_SHA='181b0378935a6df183d5044e6e42b4d26b9c5da0197b21edb80ce808eaddacc6';
+const SOURCE_SHA='cb2911f9d459b57427013000fc823d8c0b1ae9198f31320c517b81d5750c0ed2';
 export const sha256=async bytes=>Array.from(new Uint8Array(await crypto.subtle.digest('SHA-256',bytes)),x=>x.toString(16).padStart(2,'0')).join('');
 export async function buildSigningLease(env,request,values,signers,tenantValues={}){
  let source;
